34d06baee99729237b3604a1381f6ef045e618e1
rebeccaschuell <rebecca.schuell@studentfr.ch>
2023-05-11 09:46:11 +0200

    finition du slider, amélioration des détails

--- src/js/main.js
+++ src/js/main.js
@@ -2,16 +2,6 @@ import Swiper, { Navigation, Pagination } from "swiper";
 
 Swiper.use([Navigation, Pagination]);
 
-// const swiper = new Swiper(".crafts-swiper-container", {
-//   pagination: {
-//     el: ".crafts-swiper-pagination",
-//   },
-//   navigation: {
-//     nextEl: ".crafts-carousel-next",
-//     prevEl: ".crafts-carousel-prev",
-//   },
-// });
-
 const swiper = new Swiper(".swiper-container", {
   loop: true,
   pagination: {
@@ -22,34 +12,9 @@ const swiper = new Swiper(".swiper-container", {
     prevEl: ".carousel-prev",
   },
   breakpoints: {
-    768: {
+    992: {
       slidesPerView: 3,
       spaceBetween: 40,
     },
   },
 });
-
-// export {pagination} from "swiper";
-//   return (
-//     <Swiper
-//       slidesPerView={1}
-//       spaceBetween={10}
-//       breakpoints={{
-//         640: {
-//           slidesPerView: 2,
-//           spaceBetween: 20,
-//         },
-//         768: {
-//           slidesPerView: 4,
-//           spaceBetween: 40,
-//         },
-//         1024: {
-//           slidesPerView: 5,
-//           spaceBetween: 50,
-//         },
-//       }}
-//       modules={[Pagination]}
-//       className="mySwiper"
-//     >
-//   );
-// }
